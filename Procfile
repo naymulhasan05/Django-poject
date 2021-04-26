8a8a7812a0e854f2d7efd7fdcd93e8f613323be9
naymulhasan05 <naymulhasan05@gmail.com>
2021-04-26 18:19:04 +0600

    first uploading

---
+++ Procfile
@@ -0,0 +1,3 @@
+release: python manage.py migrate
+
+web: gunicorn testproject.wsgi --log-file -
--- testproject/settings.py
+++ testproject/settings.py
@@ -10,6 +10,8 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 import os
+import django_heroku
+import dj_database_url
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -129,3 +131,5 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+django_heroku.settings(locals())
